c7f0111adf584c599dd15cbd1bcaab7724aaef86
Willumpp <roperw43@gmail.com>
2024-05-07 14:40:56 +0100

    Added expandable directory of responses

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ var http = require("http");
 const path = require("path");
 var url = require("url");
 
+const PORT = 8080;
+
+/**
+ * Checks if given element is within the array
+ * @param {Iterable} array Array to check through
+ * @param {*} element Element in array to check for
+ * @returns True if element in array, otherwise false
+ */
+function isIn(array, element) {
+    let found = false;
+    
+    // !Remember! : cannot "break;" or "return;" from forEach loop!!
+    array.forEach(arrayElement => {
+        if (arrayElement == element ) { found = true; }
+    });
+
+    return found;
+}
+
 
 /**
  * Writes HTML document to response and sends it back to clinet
@@ -13,44 +32,65 @@ function sendFile(res, filePath) {
     const contentType = "text/"+path.extname(filePath).replace(".", ""); // Parse the content type using file extension
     res.writeHead(200, {'Content-Type': contentType});
 
+    console.log(`Serving file: '${filePath}'`);
+
     // Note: readFile is asynchronous
     fs.readFile(filePath, function(err, data) {
         if (err) throw err;
 
         res.write(data);
         res.end();
+        console.log(`Sent response file: '${filePath}'`);
     });
 
 }
 
-const server = http.createServer(function (req, res) {
+// Directory of actions to perform given the URL path
+const responseDirectories = {
+    // Send front page if no directory
+    "/" : function(req, res) {
+        sendFile(res, "./src/frontend/index.html");
+    },
 
+    // Send search result page
+    "/search/" : function(req, res) {
+        sendFile(res, "./src/frontend/search.html");
+    },
+
+    // Not found
+    "/notfound/" : function(req, res) {
+        console.log("Request URL not found in directory");
+        sendFile(res, "./src/frontend/error.html");
+    }
+}
+
+const server = http.createServer(function (req, res) {
+    console.log("");
     const urlParsed = url.parse(req.url, res);
     const urlPath = urlParsed.pathname; // This is the url path of the request (INC. files)
+    console.log(`Requested URL: '${urlPath}'`);
 
     
     // If specific file requested
     if (path.extname(urlPath)) {
         let newPath = "./src/frontend"+urlPath; // This is probably really unsecure!
-        console.log(`Requested file: '${newPath}'`);
-
-        // Check if the file exists
-        // If not do not send response
-        // Note: this does not work?
-        if (fs.existsSync(newPath) == false) { 
-            console.log(`Failed request: File '${newPath}' does not exist!`); 
-            return; 
-        };
-
+    
         sendFile(res, newPath);
         return;
     }
 
-    // Send front page to user
-    console.log(`Requested index.html`);
-    sendFile(res, "./src/frontend/index.html");
+    // Respond to given url
+    // If url is not in directory
+    if (isIn(Object.keys(responseDirectories), urlPath) == false) {
+        responseDirectories["/notfound/"](req, res);
+        return;
+    }
+
+    // Call respective function for URL
+    responseDirectories[urlPath](req, res);
+    
 }); 
 
-server.listen(8080, function() {
-    console.log("Now listening on port 8080");
+server.listen(PORT, function() {
+    console.log("Now listening on port "+PORT);
 });
